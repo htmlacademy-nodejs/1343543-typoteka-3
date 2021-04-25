1d1b0066eba1be0b04a6a8188ad09aa15a7bef77
Dmitry <dmitry.khalimov@gmail.com>
2021-04-25 18:55:06 +0300

    Раскрашивает выводы в консоль

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fs = require(`fs`);
+const chalk = require(`chalk`);
 
 const {
   getRandomDate,
@@ -122,10 +123,10 @@ module.exports = {
 
     fs.writeFile(FILE_NAME, content, (err) => {
       if (err) {
-        return console.error(`Can't write data to file...`);
+        return console.error(chalk.red(`Can't write data to file...`));
       }
 
-      return console.info(`Operation success. File created.`);
+      return console.info(chalk.green(`Operation success. File created.`));
     });
   }
 };
