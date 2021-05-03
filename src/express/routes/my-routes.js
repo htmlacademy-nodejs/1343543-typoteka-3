e5f5bace966a5e01869b217333389e7652bec57c
Dmitry <dmitry.khalimov@gmail.com>
2021-05-03 18:04:31 +0300

    Рефакторит

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -4,6 +4,6 @@ const {Router} = require(`express`);
 const offersRouter = new Router();
 
 offersRouter.get(`/`, (req, res) => res.send(`/my`));
-offersRouter.get(`/comments`, (req, res) => res.send(`/my/comments`));
+offersRouter.get(`/comments`, (req, res) => res.render(`my/comments`, {wrapper: `wrapper wrapper--nobackground`}));
 
 module.exports = offersRouter;
