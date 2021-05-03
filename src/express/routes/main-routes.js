bc4f0d693068be9d6fd96f8e39a4e41be39b0249
Dmitry <dmitry.khalimov@gmail.com>
2021-05-03 18:24:48 +0300

    Рефакторит

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -1,4 +1,5 @@
 'use strict';
+const {WrapperClass} = require(`../../constants`);
 
 const {Router} = require(`express`);
 const offersRouter = new Router();
@@ -6,8 +7,8 @@ const offersRouter = new Router();
 offersRouter.get(`/`, (req, res) => res.render(`main/main`));
 offersRouter.get(`/register`, (req, res) => res.render(`main/sign-up`));
 offersRouter.get(`/login`, (req, res) => res.render(`main/login`));
-offersRouter.get(`/search`, (req, res) => res.render(`main/search`, {wrapper: `wrapper-color`}));
-offersRouter.get(`/404`, (req, res) => res.render(`errors/404`, {wrapper: `wrapper-color`}));
-offersRouter.get(`/500`, (req, res) => res.render(`errors/500`, {wrapper: `wrapper-color`}));
+offersRouter.get(`/search`, (req, res) => res.render(`main/search`, {wrapper: WrapperClass.COLOR}));
+offersRouter.get(`/404`, (req, res) => res.render(`errors/404`, {wrapper: WrapperClass.COLOR}));
+offersRouter.get(`/500`, (req, res) => res.render(`errors/500`, {wrapper: WrapperClass.COLOR}));
 
 module.exports = offersRouter;
--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -1,11 +1,12 @@
 'use strict';
+const {WrapperClass} = require(`../../constants`);
 
 const {Router} = require(`express`);
 const offersRouter = new Router();
 
-offersRouter.get(`/`, (req, res) => res.render(`my/my`, {wrapper: `wrapper wrapper--nobackground`}));
-offersRouter.get(`/comments`, (req, res) => res.render(`my/comments`, {wrapper: `wrapper wrapper--nobackground`}));
+offersRouter.get(`/`, (req, res) => res.render(`my/my`, {wrapper: WrapperClass.NO_BACKGROUND}));
+offersRouter.get(`/comments`, (req, res) => res.render(`my/comments`, {wrapper: WrapperClass.NO_BACKGROUND}));
 offersRouter.get(`/new-post`, (req, res) => res.render(`my/new-post`));
-offersRouter.get(`/categories`, (req, res) => res.render(`my/categories`));
+offersRouter.get(`/categories`, (req, res) => res.render(`my/categories`, {wrapper: WrapperClass.NO_BACKGROUND}));
 
 module.exports = offersRouter;
