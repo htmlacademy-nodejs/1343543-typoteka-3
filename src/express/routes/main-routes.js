e833c7969f68f55d7a03ca65f8994f6560fc33ac
Dmitry <dmitry.khalimov@gmail.com>
2021-05-03 17:00:53 +0300

    Добавляет часть страниц

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -3,11 +3,12 @@
 const {Router} = require(`express`);
 const offersRouter = new Router();
 
-offersRouter.get(`/`, (req, res) => res.send(`/`));
-offersRouter.get(`/register`, (req, res) => res.send(`/register`));
-offersRouter.get(`/login`, (req, res) => res.send(`/login`));
-offersRouter.get(`/search`, (req, res) => res.send(`/search`));
+offersRouter.get(`/`, (req, res) => res.render(`main/main`));
+offersRouter.get(`/register`, (req, res) => res.render(`main/sign-up`));
+offersRouter.get(`/login`, (req, res) => res.render(`main/login`));
+offersRouter.get(`/search`, (req, res) => res.render(`main/search`));
 offersRouter.get(`/categories`, (req, res) => res.send(`/categories`));
 offersRouter.get(`/404`, (req, res) => res.render(`errors/404`));
+offersRouter.get(`/500`, (req, res) => res.render(`errors/500`));
 
 module.exports = offersRouter;
