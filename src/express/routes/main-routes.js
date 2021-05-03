7380f2cb178e7630ae3d41fa0fac019a9f808296
Dmitry <dmitry.khalimov@gmail.com>
2021-05-03 17:55:26 +0300

    Рефакторит

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -6,9 +6,9 @@ const offersRouter = new Router();
 offersRouter.get(`/`, (req, res) => res.render(`main/main`));
 offersRouter.get(`/register`, (req, res) => res.render(`main/sign-up`));
 offersRouter.get(`/login`, (req, res) => res.render(`main/login`));
-offersRouter.get(`/search`, (req, res) => res.render(`main/search`));
+offersRouter.get(`/search`, (req, res) => res.render(`main/search`, {wrapper: `wrapper-color`}));
 offersRouter.get(`/categories`, (req, res) => res.send(`/categories`));
-offersRouter.get(`/404`, (req, res) => res.render(`errors/404`));
-offersRouter.get(`/500`, (req, res) => res.render(`errors/500`));
+offersRouter.get(`/404`, (req, res) => res.render(`errors/404`, {wrapper: `wrapper-color`}));
+offersRouter.get(`/500`, (req, res) => res.render(`errors/500`, {wrapper: `wrapper-color`}));
 
 module.exports = offersRouter;
