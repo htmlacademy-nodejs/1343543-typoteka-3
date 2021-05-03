cef917b115ced59f9517bd7f870e9404994eb028
Dmitry <dmitry.khalimov@gmail.com>
2021-05-03 14:58:23 +0300

    Настраивает шаблонизатор

--- src/express/index.js
+++ src/express/index.js
@@ -1,13 +1,21 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
+
+const PUBLIC_DIR = `public`;
+const DEFAULT_PORT = 8080;
 
 const articlesRouter = require(`./routes/articles-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
 
-const DEFAULT_PORT = 8080;
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.set(`views`, path.resolve(__dirname, `templates/views`));
+app.set(`view engine`, `pug`);
 
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -8,5 +8,6 @@ offersRouter.get(`/register`, (req, res) => res.send(`/register`));
 offersRouter.get(`/login`, (req, res) => res.send(`/login`));
 offersRouter.get(`/search`, (req, res) => res.send(`/search`));
 offersRouter.get(`/categories`, (req, res) => res.send(`/categories`));
+offersRouter.get(`/404`, (req, res) => res.render(`errors/404`));
 
 module.exports = offersRouter;
