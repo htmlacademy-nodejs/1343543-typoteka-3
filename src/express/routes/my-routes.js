3b38fc96a42501d2de1bf55f103e951cbe1622eb
Dmitry <dmitry.khalimov@gmail.com>
2021-08-20 19:31:47 +0300

    Добавляет отрисовку данных в my

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -2,9 +2,17 @@
 const {WrapperClass} = require(`../../constants`);
 
 const {Router} = require(`express`);
+const api = require(`../api`).getAPI();
 const myRouter = new Router();
 
-myRouter.get(`/`, (req, res) => res.render(`my/my`, {wrapper: WrapperClass.NO_BACKGROUND}));
+myRouter.get(`/`, async (req, res) => {
+  // Временно отправляет моковые данные
+  const articles = await api.getArticles();
+  res.render(`my/my`, {
+    wrapper: WrapperClass.NO_BACKGROUND,
+    articles
+  });
+});
 myRouter.get(`/comments`, (req, res) => res.render(`my/comments`, {wrapper: WrapperClass.NO_BACKGROUND}));
 myRouter.get(`/new-post`, (req, res) => res.render(`my/new-post`));
 myRouter.get(`/categories`, (req, res) => res.render(`my/categories`, {wrapper: WrapperClass.NO_BACKGROUND}));
