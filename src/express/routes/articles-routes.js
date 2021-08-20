b6fc48d947f397e7b417e58569d637ac8f70c1a1
Dmitry <dmitry.khalimov@gmail.com>
2021-08-20 20:56:10 +0300

    Добавляет форму редактирования сообщения

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -3,7 +3,21 @@
 const {Router} = require(`express`);
 const articlesRouter = new Router();
 
+const api = require(`../api`).getAPI();
+
 articlesRouter.get(`/category/:id`, (req, res) => res.render(`articles/articles-by-category`));
 articlesRouter.get(`/:id`, (req, res) => res.render(`articles/post`));
+
+articlesRouter.get(`/edit/:id`, async (req, res) => {
+  const {id} = req.params;
+  const [article, categories] = await Promise.all([
+    api.getArticle(id),
+    api.getCategories()
+  ]);
+  res.render(`articles/post-edit`, {
+    article,
+    categories
+  });
+});
 
 module.exports = articlesRouter;
