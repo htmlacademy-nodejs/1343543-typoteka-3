1e37173e470251a27ea803c8e6c383a26e39bf4b
Dmitry <dmitry.khalimov@gmail.com>
2021-08-21 16:29:27 +0300

    WIP: Не работает отправка

--- src/express/api.js
+++ src/express/api.js
@@ -2,7 +2,7 @@
 
 const axios = require(`axios`);
 
-const TIMEOUT = 1000;
+const TIMEOUT = 10000;
 
 const port = process.env.API_PORT || 3000;
 const defaultUrl = `http://localhost:${port}/api/`;
@@ -14,6 +14,11 @@ class API {
       baseURL,
       timeout
     });
+  }
+
+  async _load(url, options) {
+    const response = await this._http.request({url, ...options});
+    return response.data;
   }
 
   getArticles() {
@@ -28,7 +33,7 @@ class API {
     return this._load(`/search`, {params: {query}});
   }
 
-  // А почему этот метод async?
+  // TODO а почему эти метод async а другие нет?
   async getCategories() {
     return this._load(`/category`);
   }
@@ -38,11 +43,6 @@ class API {
       method: `POST`,
       data
     });
-  }
-
-  async _load(url, options) {
-    const response = await this._http.request({url, ...options});
-    return response.data;
   }
 }
 
--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -6,7 +6,6 @@ const articlesRouter = new Router();
 const api = require(`../api`).getAPI();
 
 articlesRouter.get(`/category/:id`, (req, res) => res.render(`articles/articles-by-category`));
-articlesRouter.get(`/:id`, (req, res) => res.render(`articles/post`));
 
 articlesRouter.get(`/edit/:id`, async (req, res) => {
   const {id} = req.params;
@@ -18,6 +17,31 @@ articlesRouter.get(`/edit/:id`, async (req, res) => {
     article,
     categories
   });
+});
+
+articlesRouter.get(`/add`, async (req, res) => {
+  const categories = await api.getCategories();
+  res.render(`articles/post-add`, {categories});
+});
+
+articlesRouter.get(`/:id`, (req, res) => res.render(`articles/post`));
+
+articlesRouter.post(`/add`, async (req, res) => {
+  const articleData = {
+    category: [`Котики`],
+    title: `Дам погладить котика`,
+    announce: `Дам погладить котика`,
+    fullText: `Дам погладить котика. Дорого. Не гербалайф`,
+    createdDate: `2021-05-18T05:51:04.976Z`
+  };
+
+  try {
+    await api.createArticle(articleData);
+    res.redirect(`../my`);
+  } catch (e) {
+    console.log(e);
+    res.redirect(`back`);
+  }
 });
 
 module.exports = articlesRouter;
