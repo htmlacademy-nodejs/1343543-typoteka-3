b49374c527ac2f430692da627f32e0629c903bf3
Dmitry <dmitry.khalimov@gmail.com>
2021-08-28 21:20:21 +0300

    Правит добавление заметки

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -5,10 +5,22 @@ const {HttpCode, API_PREFIX} = require(`../../constants`);
 const routes = require(`../api`);
 const {getLogger} = require(`../lib/logger`);
 
+
 const DEFAULT_PORT = 3000;
 
 const app = express();
 const logger = getLogger({name: `api`});
+
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  logger.debug(`Request on route ${req.url}`);
+  res.on(`finish`, () => {
+    logger.info(`Response status code ${res.statusCode}`);
+  });
+  next();
+});
 
 app.use(API_PREFIX, routes);
 
@@ -20,14 +32,6 @@ app.use((req, res) => {
 
 app.use((err, _req, _res, _next) => {
   logger.error(`An error occurred on processing request: ${err.message}`);
-});
-
-app.use((req, res, next) => {
-  logger.debug(`Request on route ${req.url}`);
-  res.on(`finish`, () => {
-    logger.info(`Response status code ${res.statusCode}`);
-  });
-  next();
 });
 
 module.exports = {
