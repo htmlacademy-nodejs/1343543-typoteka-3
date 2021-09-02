ab90f1d43db74241f14ac987ed8624eabd0f92fd
Dmitry <dmitry.khalimov@gmail.com>
2021-09-02 17:11:02 +0300

    Добавляет генерацию картинок

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -15,6 +15,7 @@ const {
   MAX_ID_LENGTH,
   MAX_COMMENTS
 } = require(`../../constants`);
+const { CommentService } = require("../data-service");
 
 const FILE_NAME = `mocks.json`;
 
@@ -22,6 +23,7 @@ const FILE_SENTENCES_PATH = `./data/sentences.txt`;
 const FILE_TITLES_PATH = `./data/titles.txt`;
 const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 const FILE_COMMENTS_PATH = `./data/comments.txt`;
+const FILE_PICTURES_PATH = `./data/pictures.txt`;
 
 const DateCreation = {
   MIN: new Date(new Date().setDate(new Date().getDate() - 90)),
@@ -66,7 +68,7 @@ const generateComments = (count, comments) => (
 );
 
 const generateArticles = (params) => {
-  const {count, titles, sentences, categories, comments} = params;
+  const {count, titles, sentences, categories, comments, pictures} = params;
 
   if (count > MocksCount.MAX) {
     console.error(`Не больше 1000 публикаций`);
@@ -80,6 +82,7 @@ const generateArticles = (params) => {
     createdDate: getRandomDate(DateCreation.MIN, DateCreation.MAX),
     announce: getRandomFromList(sentences, AnnounceQuantity.MIN, AnnounceQuantity.MAX, true),
     fullText: getRandomFromList(sentences, FullQuantity.MIN, FullQuantity.MAX, true),
+    picture: getRandomFromList(pictures, 1, 1, true),
     category: getRandomFromList(categories, CategoriesQuantity.MIN, CategoriesQuantity.MAX, false),
   }));
 };
@@ -87,26 +90,37 @@ const generateArticles = (params) => {
 module.exports = {
   name: `--generate`,
   async run(args) {
+
     const readContent = async (filePath) => {
       try {
         const content = await fs.readFile(filePath, `utf8`);
-        return content.split(`\n`);
+        content.split(`\n`);
+        return content;
       } catch (err) {
         console.error(chalk.red(err));
         return [];
       }
     };
 
-    const [sentences, titles, categories, comments] = await Promise.all([
+    const [sentences, titles, categories, comments, pictures] = await Promise.all([
       readContent(FILE_SENTENCES_PATH),
       readContent(FILE_TITLES_PATH),
       readContent(FILE_CATEGORIES_PATH),
       readContent(FILE_COMMENTS_PATH),
+      readContent(FILE_PICTURES_PATH),
     ]);
+
+    // TODO - косо работает функция readContent, последний элемент - ''
+    // при этом при добавлении content.pop() она почему крашится.
+    sentences.pop();
+    titles.pop();
+    categories.pop();
+    comments.pop();
+    pictures.pop();
 
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || MocksCount.DEFAULT;
-    const content = JSON.stringify(generateArticles({count: countOffer, sentences, titles, categories, comments}));
+    const content = JSON.stringify(generateArticles({count: countOffer, sentences, titles, categories, comments, pictures}));
 
     try {
       await fs.writeFile(FILE_NAME, content);
