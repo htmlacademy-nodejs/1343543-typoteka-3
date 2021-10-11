386b6bf0d7094bf9208eb4b17c10f8e6de0d3a9a
Dmitry <dmitry.khalimov@gmail.com>
2021-10-12 00:15:09 +0300

    Выпиливает лишние сценарии из cli

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -1,19 +1,15 @@
 'use strict';
 
 const help = require(`./help`);
-const generate = require(`./generate`);
 const version = require(`./version`);
 const server = require(`./server`);
-const fill = require(`./fill`);
 const filldb = require(`./filldb`);
 
 const Cli = {
-  [generate.name]: generate,
   [filldb.name]: filldb,
   [help.name]: help,
   [version.name]: version,
   [server.name]: server,
-  [fill.name]: fill,
 };
 
 module.exports = {
