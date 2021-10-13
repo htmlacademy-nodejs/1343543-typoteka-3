ae9a9db4039d9f5deb67b362ef4441a1104b4ee3
Dmitry <dmitry.khalimov@gmail.com>
2021-10-13 13:51:42 +0300

    Чинит поиск

--- src/service/api/search.js
+++ src/service/api/search.js
@@ -15,8 +15,7 @@ module.exports = (app, service) => {
       res.status(HttpCode.BAD_REQUEST).json([]);
       return;
     }
-
-    const searchResults = service.findAll(query);
+    const searchResults = await service.findAll(query);
     const searchStatus = searchResults.length > 0 ? HttpCode.OK : HttpCode.NOT_FOUND;
 
     res.status(searchStatus)
