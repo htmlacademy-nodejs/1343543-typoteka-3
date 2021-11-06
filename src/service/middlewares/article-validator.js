73373224888166b73e0c8c178625452a80981cb1
Dmitry <dmitry.khalimov@gmail.com>
2021-11-06 13:18:44 +0300

    Добавляет валидацию публикации

--- src/service/middlewares/article-validator.js
+++ src/service/middlewares/article-validator.js
@@ -1,18 +1,46 @@
 'use strict';
 
+const Joi = require(`joi`);
 const {HttpCode} = require(`../../constants`);
 
-const articleKeys = [`title`, `createdDate`, `announce`, `fullText`, `category`];
+const ErrorOfferMessage = {
+  CATEGORIES: `Не выбрана ни одна категория статьи`,
+  TITLE_MIN: `Заголовок содержит меньше 30 символов`,
+  TITLE_MAX: `Заголовок не может содержать более 250 символов`,
+  ANNOUNCE_MIN: `Анонс публикации содержит меньше 30 символов`,
+  ANNOUNCE_MAX: `Анонс публикации не может содержать более 250 символов`,
+  TEXT_MAX: `Полный текст публикации не может содержать более 1000 символов`,
+  TYPE: `Не выбран ни один тип объявления`,
+  SUM: `Сумма не может быть меньше 100`
+};
+
+const schema = Joi.object({
+  categories: Joi.array().items(
+      Joi.number().integer().positive().messages({
+        'number.base': ErrorOfferMessage.CATEGORIES
+      })
+  ).min(1).required(),
+  title: Joi.string().min(30).max(250).required().messages({
+    'string.min': ErrorOfferMessage.TITLE_MIN,
+    'string.max': ErrorOfferMessage.TITLE_MAX
+  }),
+  announce: Joi.string().min(30).max(250).required().messages({
+    'string.min': ErrorOfferMessage.ANNOUNCE_MIN,
+    'string.max': ErrorOfferMessage.ANNOUNCE_MAX
+  }),
+  fullText: Joi.string().max(1000).messages({
+    'string.max': ErrorOfferMessage.TEXT_MAX
+  }),
+});
 
 module.exports = (req, res, next) => {
-  const newAricle = req.body;
-  const keys = Object.keys(newAricle);
-  const keysExists = articleKeys.every((key) => keys.includes(key));
+  const newArticle = req.body;
+  const {error} = schema.validate(newArticle, {abortEarly: false});
 
-  if (!keysExists) {
-    res.status(HttpCode.BAD_REQUEST)
-      .send(`Bad request`);
+  if (error) {
+    return res.status(HttpCode.BAD_REQUEST)
+      .send(error.details.map((err) => err.message).join(`\n`));
   }
 
-  next();
+  return next();
 };
