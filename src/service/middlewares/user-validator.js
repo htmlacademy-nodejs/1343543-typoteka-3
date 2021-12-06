34e648b79af6706deaa66bea2658c8b385dc89f4
Dmitry <dmitry.khalimov@gmail.com>
2021-12-06 21:04:42 +0300

    Приводит регистрацию к ТЗ

--- src/service/middlewares/user-validator.js
+++ src/service/middlewares/user-validator.js
@@ -25,8 +25,8 @@ const schema = Joi.object({
   passwordRepeated: Joi.string().required().valid(Joi.ref(`password`)).required().messages({
     'any.only': ErrorRegisterMessage.PASSWORD_REPEATED
   }),
-  avatar: Joi.string().required().messages({
-    'string.empty': ErrorRegisterMessage.AVATAR
+  avatar: Joi.string().allow(``).messages({
+    'string.empty': ErrorRegisterMessage.AVATAR,
   })
 });
 
