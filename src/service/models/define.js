0ae81ea509f5a2f5790055d08dc7229c54ee463d
Dmitry <dmitry.khalimov@gmail.com>
2021-12-10 14:52:05 +0300

    Выносит define в отдельный файл

---
+++ src/service/models/define.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const {
+  defineCategory,
+  defineComment,
+  defineArticle,
+  defineUser,
+  defineArticleCategory
+} = require(`./index`);
+
+const Alias = require(`./alias`);
+
+const defineModels = (sequelize) => {
+  const Category = defineCategory(sequelize);
+  const Comment = defineComment(sequelize);
+  const Article = defineArticle(sequelize);
+  const User = defineUser(sequelize);
+  const ArticleCategory = defineArticleCategory(sequelize);
+
+  Article.hasMany(Comment, {as: Alias.COMMENTS, foreignKey: `articleId`, onDelete: `cascade`});
+  Comment.belongsTo(Article, {as: Alias.ARTICLES, foreignKey: `articleId`});
+
+  Article.belongsToMany(Category, {through: ArticleCategory, as: Alias.CATEGORIES});
+  Category.belongsToMany(Article, {through: ArticleCategory, as: Alias.ARTICLES});
+  Article.hasMany(ArticleCategory, {as: Alias.ARTICLE_CATEGORIES});
+  Category.hasMany(ArticleCategory, {as: Alias.ARTICLE_CATEGORIES});
+
+  User.hasMany(Article, {as: Alias.ARTICLES, foreignKey: `userId`});
+  Article.belongsTo(User, {as: Alias.USERS, foreignKey: `userId`});
+
+  User.hasMany(Comment, {as: Alias.COMMENTS, foreignKey: `userId`});
+  Comment.belongsTo(User, {as: Alias.USERS, foreignKey: `userId`});
+
+  return {Category, Comment, Article, User, ArticleCategory};
+};
+
+module.exports = defineModels;
--- src/service/models/index.js
+++ src/service/models/index.js
@@ -5,32 +5,13 @@ const defineComment = require(`./comment`);
 const defineArticle = require(`./article`);
 const defineUser = require(`./user`);
 const defineArticleCategory = require(`./article-category`);
-const Alias = require(`./alias`);
 
-const define = (sequelize) => {
-  const Category = defineCategory(sequelize);
-  const Comment = defineComment(sequelize);
-  const Article = defineArticle(sequelize);
-  const User = defineUser(sequelize);
-  const ArticleCategory = defineArticleCategory(sequelize);
-
-  Article.hasMany(Comment, {as: Alias.COMMENTS, foreignKey: `articleId`, onDelete: `cascade`});
-  Comment.belongsTo(Article, {as: Alias.ARTICLES, foreignKey: `articleId`});
-
-  Article.belongsToMany(Category, {through: ArticleCategory, as: Alias.CATEGORIES});
-  Category.belongsToMany(Article, {through: ArticleCategory, as: Alias.ARTICLES});
-  Article.hasMany(ArticleCategory, {as: Alias.ARTICLE_CATEGORIES});
-  Category.hasMany(ArticleCategory, {as: Alias.ARTICLE_CATEGORIES});
-
-  User.hasMany(Article, {as: Alias.ARTICLES, foreignKey: `userId`});
-  Article.belongsTo(User, {as: Alias.USERS, foreignKey: `userId`});
-
-  User.hasMany(Comment, {as: Alias.COMMENTS, foreignKey: `userId`});
-  Comment.belongsTo(User, {as: Alias.USERS, foreignKey: `userId`});
-
-  return {Category, Comment, Article, User, ArticleCategory};
+module.exports = {
+  defineCategory,
+  defineComment,
+  defineArticle,
+  defineUser,
+  defineArticleCategory
 };
-
-module.exports = define;
 
 
